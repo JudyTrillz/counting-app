76a4f4594945af8758586d6cb827f4fc3532d134
Judy chukwu <ojudy007@gmail.comn>
2022-08-16 23:35:55 +0100

    my counting app

---
+++ passanger-counter-app/index.js
@@ -0,0 +1,57 @@
+let count = 0
+let figure = document.getElementById('count-el')
+let increase = document.getElementById('increment-btn')
+let decrease = document.getElementById('decrement-btn')
+let save = document.getElementById('save')
+let entry = document.getElementById('entries')
+let error = document.getElementById('error')
+
+
+increase.addEventListener('click', ()=>{
+    count += 1
+    figure.innerHTML = count
+})
+
+decrease.addEventListener('click', ()=>{
+    if(count <= 0){
+        count -= 0
+        figure.innerHTML = count
+    }else{
+        count -= 1
+        figure.innerHTML = count
+    }
+})
+
+save.addEventListener('click', ()=>{
+    if(count <= 0){
+        entry.classList.add('show')
+        error.classList.remove('show')
+    }else{
+        entry.classList.remove('show')
+        error.classList.add('show')
+    }
+    let saved = count + ' - '
+    entry.innerHTML += saved
+    figure.innerHTML = 0
+    count = 0
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
